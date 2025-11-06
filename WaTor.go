e73be42786d9c5fb88c126c877029b308fdbbcf3
Ryan-Dunne <c00263405@itcarlow.ie>
2025-11-06 00:44:47 +0000

    Display sharks & fish

--- WaTor.go
+++ WaTor.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"image/color"
 	"log"
 	"math/rand"
@@ -12,15 +13,15 @@ import (
 type Game struct{}
 
 const scale int = 5 //Size of entities
-const width = 600
-const height = 600
+const width = 800
+const height = 800
 
 var blue color.Color = color.RGBA{69, 145, 196, 255}
 var yellow color.Color = color.RGBA{255, 230, 120, 255}
 var red color.Color = color.RGBA{255, 0, 0, 255}
 
-var NumShark = 80   //Starting Shark Population
-var NumFish = 150   //Starting Fish Population
+var NumShark = 500  //Starting Shark Population
+var NumFish = 650   //Starting Fish Population
 var FishBreed = 10  //Num of chronons that must pass before fish can breed
 var SharkBreed = 12 //Num of chronons that must pass before sharks can breed
 var Starve = 15     //Num of time sharks can go without food before death
@@ -36,23 +37,22 @@ func Chronon() {
 }
 
 func (g *Game) Update() error {
-	Chronon()
+	//Chronon()
 	for x := 1; x < width-1; x++ { //Across One pixel, when column complete
 		for y := 1; y < height-1; y++ { //Iterates column
-			buffer[x][y] = 0 //Buffer defaults to no entity
 
-			n := grid[x-1][y+0] + //Checks the neighbourhood - W,S,N,E
-				grid[x+0][y-1] +
-				grid[x+0][y+1] +
-				grid[x+1][y+0]
+			//	n := grid[x-1][y+0] + //Checks the neighbourhood - W,S,N,E
+			//		grid[x+0][y-1] +
+			//		grid[x+0][y+1] +
+			//		grid[x+1][y+0]
 
-			if grid[x][y] == 0 && n == 3 { //If tile empty & 3 entities surround
-				buffer[x][y] = 1 //Create new entity
-			} else if n < 2 || n > 3 { //If there's too few or too many entities, leave current cell blank
-				buffer[x][y] = 0
-			} else {
-				buffer[x][y] = grid[x][y]
-			}
+			//	if grid[x][y] == 0 && n == 3 { //If tile empty & 3 entities surround
+			//		buffer[x][y] = 1 //Create new entity
+			//	} else if n < 2 || n > 3 { //If there's too few or too many entities, leave current cell blank
+			//		buffer[x][y] = 0
+			//	} else {
+			//		buffer[x][y] = grid[x][y]
+			//	}
 		}
 	}
 
@@ -70,7 +70,9 @@ func (g *Game) Draw(window *ebiten.Image) {
 			for i := 0; i < scale; i++ {
 				for j := 0; j < scale; j++ {
 					if grid[x][y] == 1 { //If there's an entity
-						window.Set(x*scale+i, y*scale+j, red) //Change pixel colour
+						window.Set(x*scale+i, y*scale+j, yellow) //Change pixel colour
+					} else if grid[x][y] == 2 {
+						window.Set(x*scale+i, y*scale+j, red)
 					}
 				}
 			}
@@ -83,8 +85,10 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func main() {
+	setupFish()
+	setupShark()
 	game := &Game{}
-	ebiten.SetWindowSize(1280, 720)
+	ebiten.SetWindowSize(800, 800)
 	ebiten.SetWindowTitle("My Game")
 
 	if err := ebiten.RunGame(game); err != nil {
@@ -94,6 +98,32 @@ func main() {
 
 func setupFish() {
 	for i := 0; i < NumFish; i++ {
-		rand.Int() //Generate random coords to spawn fishs
+		fishSpawnX := rand.Intn(800) //Generate random coords to spawn fish
+		fishSpawnY := rand.Intn(800)
+		buffer[fishSpawnX][fishSpawnY] = 1
+
+	}
+	printGrid()
+}
+
+func setupShark() {
+	for i := 0; i < NumShark; i++ {
+		sharkSpawnX := rand.Intn(800) //Generate random coords to spawn fish
+		sharkSpawnY := rand.Intn(800)
+		if buffer[sharkSpawnX][sharkSpawnY] != 0 {
+			i--
+			continue
+		}
+		buffer[sharkSpawnX][sharkSpawnY] = 2
+	}
+	printGrid()
+}
+
+func printGrid() {
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			fmt.Printf("%d ", buffer[x][y])
+		}
+		fmt.Println()
 	}
 }
